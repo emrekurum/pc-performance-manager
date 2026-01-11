a3de476b463c8476e45af9fbdeed5aca70cfc29a
Emre KURUM <emrekurum07@hotmail.com>
2026-01-11 04:17:48 +0300

    Add ServiceView UI for Windows Services management

---
+++ Views/ServiceView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace PcPerformanceManager.Views;
+
+public partial class ServiceView : UserControl
+{
+    public ServiceView()
+    {
+        InitializeComponent();
+    }
+}
