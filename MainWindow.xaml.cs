233d02c947d3e10894fd5f0b42f19417322c8caa
Emre KURUM <emrekurum07@hotmail.com>
2026-01-16 23:01:34 +0300

    feat: Add settings loading and application in MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using PcPerformanceManager.Models;
 using PcPerformanceManager.ViewModels;
 
 namespace PcPerformanceManager;
@@ -8,5 +10,27 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        LoadSettings();
+    }
+
+    private void LoadSettings()
+    {
+        try
+        {
+            var settingsService = new Services.SettingsService();
+            var settings = settingsService.LoadSettingsAsync().GetAwaiter().GetResult();
+            ApplySettings(settings);
+        }
+        catch
+        {
+            // Settings yüklenemezse varsayılan değerler kullanılır
+        }
+    }
+
+    public void ApplySettings(AppSettings settings)
+    {
+        // Tema değiştirme (şimdilik sadece dark tema destekleniyor)
+        // Gelecekte light tema desteği eklenebilir
+        // Not: Minimize to tray özelliği şimdilik devre dışı (UseWindowsForms gerektiriyor)
     }
 }
